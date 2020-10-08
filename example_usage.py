962d963a05442c7c6605cf3127e9bda12d38a78b
Sebastian Monnet <sebastian.monnet@gmail.com>
2020-10-08 17:57:55 +0100

    Updated usage_example

--- example_usage.py
+++ example_usage.py
@@ -2,25 +2,6 @@ from get_raw_path import *
 from trace_path import *
 
 
-path, vid = get_path(1)
+vid, left, right = get_series(1)
 
-play_vid(vid, wait=30)
-
-plt.imshow(path)
-plt.show()
-
-filtered = filter_isolated(path)
-left, right = split_series(filtered)
-
-plt.scatter(left[0], left[1])
-plt.scatter(right[0], right[1])
-
-plt.show()
-
-
-left = remove_outliers(left[0], left[1])
-right = remove_outliers(right[0], right[1])
-
-plt.scatter(left[0], left[1])
-plt.scatter(right[0], right[1])
-plt.show()
+play_vid(vid)
